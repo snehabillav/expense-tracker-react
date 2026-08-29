2afd73d709b67ef238de2873e19ece19f2697571
Sneha Billav <snehabillav@gmail.com>
2026-08-29 20:20:11 +0530

    Add nested routing dashboard practice

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,32 @@
 import { Routes, Route } from "react-router-dom";
-import { useParams } from "react-router-dom";
+import { Outlet } from "react-router-dom";
 
-function ProductDetails() {
-  const id = useParams();
-  return <h1>Product ID: {id.id}</h1>
+function Dashboard() {
+    return(
+        <div>
+            <h1>Dashboard</h1>
+            <Outlet />
+        </div>
+    );
+}
+
+function Profile() {
+    return <h2>Profile</h2>
+}
+
+function Settings() {
+    return <h2>Settings</h2>
 }
 
 function App() {
-  return (
-    <Routes>
-      <Route
-        path="/products/:id" element={<ProductDetails />} />
-    </Routes>
-  );
+    return (
+        <Routes>
+            <Route path="/dashboard" element={<Dashboard />}>
+                <Route path="profile" element={<Profile />} />
+                <Route path="settings" element={<Settings />} />
+            </Route>
+        </Routes>
+    );
 }
 
 export default App;
