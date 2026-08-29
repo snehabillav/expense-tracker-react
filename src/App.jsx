c128ff3501ac62e2b70aa3d11c4500f4a718a444
Sneha Billav <snehabillav@gmail.com>
2026-08-29 17:09:42 +0530

    Added dynamic routes and useParams

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,17 @@
 import { Routes, Route } from "react-router-dom";
-import { Outlet } from "react-router-dom";
+import { useParams } from "react-router-dom";
 
-
-function Products() {
-  return (
-  <div>
-    <h1>Products</h1>
-    <Outlet />
-
-    </div>
-  );
-}
-
-function Phones() {
-  return <h2>Phones</h2>;
+function ProductDetails() {
+  const id = useParams();
+  return <h1>Product ID: {id.id}</h1>
 }
 
 function App() {
   return (
-<Routes>
-  <Route path="/products" element={<Products />}>
-  <Route path="phones" element={<Phones />}>
-  </Route>
-  </Route>
-</Routes>
+    <Routes>
+      <Route
+        path="/products/:id" element={<ProductDetails />} />
+    </Routes>
   );
 }
 
