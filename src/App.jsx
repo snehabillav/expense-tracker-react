37af7c40015235cd57a2146c97df7be6bdc65eed
Sneha Billav <snehabillav@gmail.com>
2026-08-31 00:08:27 +0530

    Completed React Router product app

--- src/App.jsx
+++ src/App.jsx
@@ -1,83 +1,70 @@
 import { Routes, Route } from "react-router-dom";
-import { Link, NavLink } from "react-router-dom";
-import { useNavigate } from "react-router-dom";
+import { Link } from "react-router-dom";
+import { useParams } from "react-router-dom";
+import { Outlet } from "react-router-dom";
+
+
 
 
 function Home() {
     return (
-    <div>
-        <h1>Home</h1>
-        <Link to= "/about"> Go to About </Link>
-        <Link to= "/contact"> Go to Contact </Link>
-        <Link to= "/login"> Go to Login </Link>
-    </div>
+        <div>
+            <h1>Home</h1>
+            <Link to="/products"> Go to Products</Link>
+        </div>
     );
 }
 
-function About() {
+function Products() {
     return (
-    <div>
-        <h1>About</h1>
-        <Link to= "/"> Back to Home </Link>
-    </div>
-  );
-}
+        <div>
+            <h1>Products</h1>
+            <Link to="/products/101"> Product 101 </Link>
+            <Link to="/products/102"> Product 102 </Link>
+            <Link to="/products/103"> Product 103 </Link>
 
-function Contact() {
-    return(
-    <div>
-        <h1>Contact</h1>
-        <NavLink 
-        to= "/"
-        className={({ isActive }) => isActive ? "active" : ""}
-        > 
-        Home 
-        </NavLink>
-
-        <NavLink 
-        to= "/about"
-        className={({ isActive }) => isActive ? "active" : ""}
-        >
-             About
-              </NavLink>
+            <Link to="/products/phones"> Phones </Link>
+            <Link to="/products/laptops"> Laptops </Link>
 
 
-        <NavLink
-         to= "/login"
-         className={({ isActive }) => isActive ? "active" : ""}
-         > 
-         Login 
-         </NavLink>
-    </div>
+            <Outlet />
+        </div>
     );
 }
 
-function Login() {
-    const navigate = useNavigate(); 
-    
-    function handleLogin() {
-        navigate("/")
-    }
-        return(
-            <div>
-                <h1>Login</h1>
-                <button onClick={handleLogin}>
-                    Login
-                </button>
-            </div>
-        );
-    }
+function Phones() {
+    return <h1>Phones</h1>
+}
 
-    
+function Laptops() {
+    return <h1>Laptops</h1>
+}
+
+
+function ProductDetails() {
+    const id = useParams();
+    return (
+        <div>
+            <h1>Product Details</h1>
+            <p>Product ID: {id.id}</p>
+        </div>
+    );
+}
+
+
 
 function App() {
-    return (
-        <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/about" element={<About />} />
-            <Route path="/contact" element={<Contact />} />
-            <Route path="/login" element={<Login />} />
-        </Routes>
+    return(
+        <div>
+            <Routes>
+                <Route path="/" element={<Home />} />
+                <Route path="/products/:id" element={<ProductDetails />} />
+                <Route path="/products" element={<Products />}>
+                    <Route path="phones" element={<Phones />} />
+                    <Route path="laptops" element={<Laptops />} />
+                </Route>
+            </Routes>
+        </div>
     )
 }
 
