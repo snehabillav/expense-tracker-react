8675df58d08ec4dbf52c7306113aed9770e66a85
Sneha Billav <snehabillav@gmail.com>
2026-08-31 14:52:21 +0530

    Completed student dashboard project

--- src/App.jsx
+++ src/App.jsx
@@ -1,56 +1,104 @@
 import { Routes, Route } from "react-router-dom";
-import { Link } from "react-router-dom";
+import { Link, NavLink } from "react-router-dom";
 import { useParams } from "react-router-dom";
 import { Outlet } from "react-router-dom";
-
-
+import { useNavigate } from "react-router-dom";
+import "./App.css";
 
 
 function Home() {
-    return (
+    return(
         <div>
             <h1>Home</h1>
-            <Link to="/products"> Go to Products</Link>
+            
+            <NavLink
+                to="/"
+                className={({ isActive }) => isActive ? "active" : ""}
+            >
+                Home
+            </NavLink>
+
+            <NavLink
+                to="/students"
+                className={({ isActive }) => isActive ? "active" : ""}
+            >
+                Students
+            </NavLink>
+
+            <NavLink
+                to="/profile"
+                className={({ isActive }) => isActive ? "active" : ""}
+            >
+                Profile
+            </NavLink>
+
+           <NavLink
+                to="/courses"
+                className={({ isActive }) => isActive ? "active" : ""}
+            >
+                Courses
+            </NavLink>
         </div>
     );
 }
 
-function Products() {
-    return (
+function Students() {
+    return(
         <div>
-            <h1>Products</h1>
-            <Link to="/products/101"> Product 101 </Link>
-            <Link to="/products/102"> Product 102 </Link>
-            <Link to="/products/103"> Product 103 </Link>
+            <h1>Students</h1>
+            <Link to="/students/101"> Student 101 </Link>
+            <Link to="/students/102"> Student 102 </Link>
+            <Link to="/students/103"> Student 103 </Link>
+        </div>
+    );
+}
 
-            <Link to="/products/phones"> Phones </Link>
-            <Link to="/products/laptops"> Laptops </Link>
+function StudentDetails() {
+    const id = useParams();
+    const navigate = useNavigate();
 
+    function handleBack() {
+        navigate("/students")
+    }
+    return(
+        <div>
+            <h1>Student Details</h1>
+            <p>Student ID: {id.id}</p>  
+
+            <button onClick={handleBack}>
+                Back to Students
+                </button> 
+
+    
+        </div>
+    );
+}
+
+function Courses() {
+    return(
+        <div>
+            <h1>Courses</h1>
+            <Link to="/courses/javascript"> JavaScript </Link>
+            <Link to="/courses/react"> React </Link>
 
             <Outlet />
+
         </div>
     );
 }
 
-function Phones() {
-    return <h1>Phones</h1>
-}
-
-function Laptops() {
-    return <h1>Laptops</h1>
+function Profile() {
+    return <h1>Profile</h1>
 }
 
 
-function ProductDetails() {
-    const id = useParams();
-    return (
-        <div>
-            <h1>Product Details</h1>
-            <p>Product ID: {id.id}</p>
-        </div>
-    );
+function JavaScript() {
+    return <h1>JavaScript</h1>
 }
 
+function React() {
+    return <h1>React</h1>
+}
 
 
 function App() {
@@ -58,14 +106,16 @@ function App() {
         <div>
             <Routes>
                 <Route path="/" element={<Home />} />
-                <Route path="/products/:id" element={<ProductDetails />} />
-                <Route path="/products" element={<Products />}>
-                    <Route path="phones" element={<Phones />} />
-                    <Route path="laptops" element={<Laptops />} />
-                </Route>
-            </Routes>
+                <Route path="/students" element={<Students />} />
+                <Route path="/students/:id" element={<StudentDetails />} />
+                <Route path="/profile" element={<Profile />} />
+            <Route path="/courses" element={<Courses />} >
+                 <Route path="javascript" element={<JavaScript />} />
+                 <Route path="react" element={<React />} />
+            </Route>
+              </Routes>
         </div>
-    )
+    );
 }
 
 export default App;
